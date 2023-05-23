552b5ba136d34d3528a085128161eb13a3a10396
k.stopczynska <k.stopczynska@gmail.com>
2023-05-23 17:30:42 +0200

    adding drop styles on board and actions for audio icon

--- client/src/components/nav/Audio.tsx
+++ client/src/components/nav/Audio.tsx
@@ -1,34 +1,37 @@
-import { useState, useRef, useEffect, useCallback } from "react";
-import { assetsURL } from "../../utils/utils";
-import "./Audio.css";
+import { useState, useRef, useEffect, useCallback } from 'react';
+import { assetsURL } from '../../utils/utils';
+import './Audio.css';
 
 interface AudioProps {
-    url: string;
-    loop: boolean;
+	url: string;
+	loop: boolean;
 }
 
 export const Audio = (props: AudioProps) => {
-    const { url, loop } = props;
-    const [play, setPlay] = useState(false);
+	const { url, loop } = props;
+	const [play, setPlay] = useState(false);
 
-    const audioRef = useRef() as any;
+	const audioRef = useRef() as any;
 
-    const soundToggle = useCallback(() => {
-        setPlay((prev) => !prev);
-    }, []);
+	const soundToggle = useCallback(() => {
+		setPlay((prev) => !prev);
+	}, []);
 
-    useEffect(() => {
-        if (audioRef) {
-            play ? audioRef.current.play() : audioRef.current.pause();
-        }
-    }, [play]);
+	useEffect(() => {
+		if (audioRef) {
+			play ? audioRef.current.play() : audioRef.current.pause();
+		}
+	}, [play]);
 
-    return (
-        <>
-            <button className="sound-icon" onClick={soundToggle}>
-                <audio src={url} loop={loop} ref={audioRef}></audio>
-                <img src={assetsURL.soundIcon} alt="audio" />
-            </button>
-        </>
-    );
+	return (
+		<>
+			<button
+				className={!play ? 'sound-icon' : 'sound-icon-active'}
+				onClick={soundToggle}
+			>
+				<audio src={url} loop={loop} ref={audioRef}></audio>
+				<img src={assetsURL.soundIcon} alt='audio' />
+			</button>
+		</>
+	);
 };
--- client/src/components/nav/Header.tsx
+++ client/src/components/nav/Header.tsx
@@ -1,58 +1,56 @@
-import { useState, useContext, useCallback } from "react";
-import { v4 as uuidv4 } from "uuid";
-import { GameContext } from "../../store/gameStore";
-import { Button } from "../../UI/Button";
-import { Audio } from "./Audio";
-import { assetsURL } from "../../utils/utils";
-import "./Header.css";
+import { useState, useContext, useCallback } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import { GameContext } from '../../store/gameStore';
+import { Button } from '../../UI/Button';
+import { Audio } from './Audio';
+import { assetsURL } from '../../utils/utils';
+import './Header.css';
 
 interface HeaderProps {
-    connect: () => void;
-    disconnect: () => void;
+	connect: () => void;
+	disconnect: () => void;
 }
 
 export const Header = (props: HeaderProps) => {
-    const { connect, disconnect } = props;
-    const [loggedIn, setLoggedIn] = useState(false);
-    const { setPlayerId } = useContext(GameContext);
+	const { connect, disconnect } = props;
+	const [loggedIn, setLoggedIn] = useState(false);
+	const { setPlayerId } = useContext(GameContext);
 
-    const logInHandler = useCallback(() => {
-        setLoggedIn(true);
-        const id = uuidv4();
-        setPlayerId(id);
-        connect();
-    }, []);
+	const logInHandler = useCallback(() => {
+		setLoggedIn(true);
+		const id = uuidv4();
+		setPlayerId(id);
+		connect();
+	}, []);
 
-    const logOutHandler = useCallback(() => {
-        setLoggedIn(false);
-        setPlayerId("");
-        disconnect();
-    }, []);
+	const logOutHandler = useCallback(() => {
+		setLoggedIn(false);
+		setPlayerId('');
+		disconnect();
+	}, []);
 
-    return (
-        <nav className="header">
-            <a
-                href="https://www.roulettesites.org/rules/"
-                rel="noreferrer"
-                target="blank"
-            >
-                <Button className="logo">
-                    <h1 className="main-heading">Magic roulette</h1>
-                </Button>
-            </a>
-            <div className="audio-login-container">
-                {!loggedIn && (
-                    <Button className="login-button" onClick={logInHandler}>
-                        Log In
-                    </Button>
-                )}
-                {loggedIn && (
-                    <Button className="logout-button" onClick={logOutHandler}>
-                        Log Out
-                    </Button>
-                )}
-                <Audio url={assetsURL.soundtrack} loop={true} />
-            </div>
-        </nav>
-    );
+	return (
+		<nav className='header'>
+			<a
+				href='https://www.roulettesites.org/rules/'
+				rel='noreferrer'
+				target='blank'
+			>
+				<Button className='logo'>Magic roulette</Button>
+			</a>
+			<div className='audio-login-container'>
+				{!loggedIn && (
+					<Button className='login-button' onClick={logInHandler}>
+						Log In
+					</Button>
+				)}
+				{loggedIn && (
+					<Button className='logout-button' onClick={logOutHandler}>
+						Log Out
+					</Button>
+				)}
+				<Audio url={assetsURL.soundtrack} loop={true} />
+			</div>
+		</nav>
+	);
 };
