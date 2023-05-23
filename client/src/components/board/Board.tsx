166682fe2d0e7bfb0e71ebc0ef3ab75e4059435b
k.stopczynska <k.stopczynska@gmail.com>
2023-05-23 15:09:51 +0200

    drop styling added

--- client/src/components/board/Board.tsx
+++ client/src/components/board/Board.tsx
@@ -1,63 +1,63 @@
-import { useCallback } from "react";
-import { useDrop } from "react-dnd";
-import { gameStore } from "../../store/gameStore";
-import { BoardItem } from "./BoardItem";
-import { Chip } from "./Chip";
-import { matrix } from "../../utils/utils";
-import { Bet } from "../../../../common/types";
-import "./Board.css";
+import { useCallback } from 'react';
+import { useDrop } from 'react-dnd';
+import { gameStore } from '../../store/gameStore';
+import { BoardItem } from './BoardItem';
+import { Chip } from './Chip';
+import { matrix } from '../../utils/utils';
+import { Bet } from '../../../../common/types';
+import './Board.css';
 
 export const Board = () => {
-    // eslint-disable-next-line
-    const [{ isOver }, drop] = useDrop(() => ({
-        accept: "chips",
-        drop: (item: string | number, monitor) => {
-            const location = monitor.getClientOffset();
-            if (location) {
-                const elem = document.elementFromPoint(location.x, location.y)!;
-                return {
-                    name: elem.id,
-                    location: location,
-                };
-            }
-        },
-        collect: (monitor) => ({
-            isOver: !!monitor.isOver({ shallow: false }),
-        }),
-    }));
+	// eslint-disable-next-line
+	const [{ isOver }, drop] = useDrop(() => ({
+		accept: 'chips',
+		drop: (item: string | number, monitor) => {
+			const location = monitor.getClientOffset();
+			if (location) {
+				const elem = document.elementFromPoint(location.x, location.y)!;
+				return {
+					name: elem.id,
+					location: location,
+				};
+			}
+		},
+		collect: (monitor) => ({
+			isOver: !!monitor.isOver({ shallow: false }),
+		}),
+	}));
 
-    const trimItem = useCallback((item: string | number) => {
-        return typeof item === "string"
-            ? item.includes("_")
-                ? item.split("_")[0]
-                : item
-            : item;
-    }, []);
+	const trimItem = useCallback((item: string | number) => {
+		return typeof item === 'string'
+			? item.includes('_')
+				? item.split('_')[0]
+				: item
+			: item;
+	}, []);
 
-    return (
-        <div ref={drop} className="board-grid">
-            {matrix.map((row) =>
-                row.map((tableItem) => (
-                    <BoardItem
-                        tableItem={trimItem(tableItem)}
-                        key={tableItem}
-                        id={tableItem}
-                    ></BoardItem>
-                )),
-            )}
-            {gameStore.bets !== null &&
-                gameStore.bets.map((bet: Bet) => (
-                    <Chip
-                        id={bet.betChips[0].id}
-                        alt={bet.betChips[0].alt}
-                        url={bet.betChips[0].url}
-                        key={Math.random()}
-                        style={{
-                            top: bet.betLocation.y,
-                            left: bet.betLocation.x,
-                        }}
-                    />
-                ))}
-        </div>
-    );
+	return (
+		<div ref={drop} className='board-grid'>
+			{matrix.map((row) =>
+				row.map((tableItem) => (
+					<BoardItem
+						tableItem={trimItem(tableItem)}
+						key={tableItem}
+						id={tableItem}
+					></BoardItem>
+				)),
+			)}
+			{gameStore.bets !== null &&
+				gameStore.bets.map((bet: Bet) => (
+					<Chip
+						id={bet.betChips[0].id}
+						alt={bet.betChips[0].alt}
+						url={bet.betChips[0].url}
+						key={Math.random()}
+						style={{
+							top: bet.betLocation.y,
+							left: bet.betLocation.x,
+						}}
+					/>
+				))}
+		</div>
+	);
 };
--- client/src/components/board/BoardItem.tsx
+++ client/src/components/board/BoardItem.tsx
@@ -1,37 +1,68 @@
-import { useCallback } from "react";
-import { observer } from "mobx-react";
-import { REDS, BLACKS } from "../../utils/utils";
-import "./BoardItem.css";
+import { useCallback } from 'react';
+import { observer } from 'mobx-react';
+import { useDrop } from 'react-dnd';
+import { REDS, BLACKS } from '../../utils/utils';
+import './BoardItem.css';
 
 interface BoardItemProps {
-    children?: React.ReactNode;
-    tableItem: number | string;
-    id: number | string;
+	children?: React.ReactNode;
+	tableItem: number | string;
+	id: number | string;
 }
 
 export const BoardItem = observer((props: BoardItemProps) => {
-    const { tableItem, id } = props;
+	const { tableItem, id } = props;
 
-    const getClassName = useCallback(() => {
-        if (typeof tableItem === "number") {
-            return REDS.includes(tableItem)
-                ? "item red"
-                : BLACKS.includes(tableItem)
-                ? "item black"
-                : "item plain";
-        }
-        if (typeof tableItem === "string") {
-            return tableItem === "RED"
-                ? "item red"
-                : tableItem === "BLACK"
-                ? "item black"
-                : "item plain";
-        }
-    }, []);
+	const [{ isOver }, drop] = useDrop(() => ({
+		accept: 'chips',
+		drop: (item: string | number, monitor) => {
+			     const location = monitor.getClientOffset();
+					if (location) {
+						const elem = document.elementFromPoint(
+							location.x,
+							location.y,
+						)!;
+						return {
+							name: elem.id,
+							location: location,
+						};
+					}
+		},
+		collect: (monitor) => ({
+			isOver: !!monitor.isOver({ shallow: true }),
+		}),
+	}));
 
-    return (
-        <div className={getClassName()} id={id.toString()}>
-            {tableItem}
-        </div>
-    );
+	const getClassName = useCallback(() => {
+		if (typeof tableItem === 'number') {
+			return REDS.includes(tableItem)
+				? 'item red'
+				: BLACKS.includes(tableItem)
+				? 'item black'
+				: 'item plain';
+		}
+		if (typeof tableItem === 'string') {
+			return tableItem === 'RED'
+				? 'item red'
+				: tableItem === 'BLACK'
+				? 'item black'
+				: 'item plain';
+		}
+	}, []);
+
+	return (
+		<div
+			ref={drop}
+			className={getClassName()}
+			id={id.toString()}
+			style={{
+				border: isOver ? '1px solid rgb(255, 173, 0)' : '1px solid white',
+				boxShadow: isOver
+					? '0 0 0.7em 0 rgb(255, 173, 0), inset 0 0 0.7em 0 rgb(255, 173, 0)'
+					: 'none',
+			}}
+		>
+			{tableItem}
+		</div>
+	);
 });
